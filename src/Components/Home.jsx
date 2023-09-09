b56cee5c3746d94544ffdc6d86c1f32a9632b30d
Clifford Dle Arnejo <83800514+freddy3rd@users.noreply.github.com>
2023-09-09 12:07:57 +0800

    update_component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,7 +6,7 @@ import Navigation from "./Navigation";
 
 function Home() {
   return (
-    <div className="flex flex-col items-center w-full h-screen justify-center px-4">
+    <div className="flex flex-col items-center w-full h-screen justify-center">
       <div className="flex flex-col gap-4 w-full max-w-[1200px ] shadow-lg p-5 rounded-md">
         <Navigation />
         <WeatherCalendar />
